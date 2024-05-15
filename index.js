7f22aa782de6d0cd38b072be335384f0e3d6fec5
unknown <anisamim94@gmail.com>
2024-05-15 23:31:01 +0600

    added a vercel

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ const corsOptions = {
     origin: [
         'http://localhost:5173',
         'http://localhost:5174',
-        'https://solosphere.web.app',
+        'https://herritage-1b537.web.app',
+        'https://herritage-1b537.firebaseapp.com',
+        'https://glistening-twilight-b2f2a1.netlify.app/',
+
     ],
     credentials: true,
     optionSuccessStatus: 200,
@@ -22,9 +25,9 @@ const corsOptions = {
 
 //midleware
 
-app.use(cors(corsOptions))
-app.use(express.json())
-app.use(cookieParser())
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
 
 
 
@@ -43,10 +46,49 @@ const client = new MongoClient(uri, {
     }
 });
 
+// jwt verify middleware
+// const logger = (req, res, next) => {
+//     console.log('called', req.host, req.originalUrl);
+//     next();
+// }
+
+// const verifyToken = (req, res, next) => {
+//     const token = req.cookies?.token;
+//     console.log('value of token middlewere', token)
+//     //if not token
+//     if (!token) {
+//         return res.status(401).send({ message: 'unauthorized2' });
+//     }
+//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//         console.log('err', err)
+//         //err
+//         if (err) {
+//             return res.status(401).send({ message: 'unauthorized1' });
+//         }
+//         console.log('decoded', decoded)
+//         req.user = decoded;
+//         next()
+
+
+//     })
+
+// }
+
+
+const cookieOption = {
+
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production" ? true : false,
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+}
+
+
+
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const roomsCollection = client.db("roomsDB").collection("rooms");
         const bookingsCollection = client.db("roomsDB").collection("bookings");
@@ -54,20 +96,15 @@ async function run() {
 
         //Auth related Api
 
-        app.post('/jwt', async (req, res) => {
-            const user = req.body;
-            console.log(user)
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-            res
-                .cookie('token', token, {
-                    httpOnly: true,
-                    secure: true,
-                    sameSite: 'none',
+        // app.post('/jwt', async (req, res) => {
+        //     const user = req.body;
+        //     // console.log(user)
+        //     const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '48h' });
+        //     res
+        //         .cookie('token', token, cookieOption)
 
-                })
-
-                .send({ success: true });
-        })
+        //         .send({ success: true });
+        // })
 
 
 
@@ -107,7 +144,14 @@ async function run() {
 
         //get some bookings data from db
         app.get('/bookings', async (req, res) => {
-            console.log('token here', req.cookies.token)
+            // console.log('token here', req.cookies.token)
+            console.log('valid token user', req.user);
+
+            // if (req.query?.email !== req.user?.email) {
+            //     return res.status(403).send({ message: 'forbidden access' });
+
+            // }
+
             let query = {};
             if (req.query?.email) {
                 query = { email: req.query.email }
@@ -185,7 +229,7 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
